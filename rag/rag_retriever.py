89cd07c4cec85136d564a2ed67daf1c12b90c002
Sai Nithish Thagarasapu <sai_nithish_chowdary.thagarasapu@mailbox.tu-dresden.de>
2025-05-17 17:28:44 +0200

    Created a working rag database and rag retriever

---
+++ rag/rag_retriever.py
@@ -0,0 +1,29 @@
+from langchain_community.vectorstores import FAISS
+from utils import embedding_model
+
+
+def retrieve_context_from_vector_database(input_query, folder_path, index_dir, score_threshold_limit=0.3):
+
+    embed_model = embedding_model()
+
+    vector_store = FAISS.load_local(
+        folder_path=folder_path,
+        index_name=index_dir, embeddings=embed_model , allow_dangerous_deserialization=True
+    )
+
+    retriever = vector_store.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={'score_threshold': score_threshold_limit}
+    )
+
+    result = retriever.invoke(input_query)
+    print(f"retrievede")
+
+
+input_query = "what are the main drugs used?"
+index_dir = "index"
+folder_path = "/Users/NithishChowdary1/Desktop/Workspace/Innovate-a-thon/doc-chat/rag/faiss_index"
+retrieve_context_from_vector_database(input_query, folder_path, index_dir)
+
+
+
